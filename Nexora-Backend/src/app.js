15b3b1321f2bcc48b68515f798962ad92530209f
Arpith Paliwal <arpithpaliwal@gmail.com>
2025-11-30 13:16:15 +0530

    Refactor CORS middleware to allow dynamic origin handling

--- Nexora-Backend/src/app.js
+++ Nexora-Backend/src/app.js
@@ -3,13 +3,17 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 const app = express();
-
-app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    })
-);
+const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS blocked"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
